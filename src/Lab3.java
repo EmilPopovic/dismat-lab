9673528c8609e3371399312e53e333cbd4ff4f56
Emil Popović <104315710+EmilPopovic@users.noreply.github.com>
2025-01-21 12:02:27 +0100

    lab3 refactor

--- src/Lab3.java
+++ src/Lab3.java
@@ -20,91 +20,85 @@ public class Lab3 {
     }
 
     public static void main(String[] args) {
-        String filePath;
+        System.out.print("Unesite ime datoteke: ");
+        String path;
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Unesite ime datoteke: ");
-            filePath = "src/" + scanner.nextLine();
+            path = "src/" + scanner.nextLine();
         }
 
-        FileData fileData = new FileData();
+        FileData data = new FileData();
         try {
-            fileData = processFile(filePath);
+            data = processFile(path);
         } catch (FileNotFoundException e) {
-            System.out.println("Nepravilna datoteka: " + filePath);
+            System.out.println("Nepravilna datoteka: " + path);
             System.exit(1);
         } catch (Exception _) {
-            System.out.println("Nepravilno formatirana datoteka: " + filePath);
+            System.out.println("Nepravilno formatirana datoteka: " + path);
             System.exit(1);
         }
 
-        System.out.println("Rjesenje: " + (isKColorable(fileData.graph, fileData.k) ? 1 : 0));
+        System.out.println("Rjesenje: " + (isKColorable(data.graph, data.k) ? 1 : 0));
     }
 
     public static boolean isKColorable(List<Vert> graph, int k) {
-        if (graph.size() <= k) {
+        if (graph.size() <= k)
             return true;
-        }
         
-        graph.sort((v1, v2) -> v2.degree - v1.degree);
+        graph.sort((v, w) -> w.degree - v.degree);
 
         boolean[] usedColors = new boolean[k + 1];
 
         return colorGraph(graph, 0, k, usedColors);
     }
 
-    private static boolean colorGraph(List<Vert> graph, int index, int k, boolean[] usedColors) {
-        if (index >= graph.size()) {
+    private static boolean colorGraph(List<Vert> graph, int i, int k, boolean[] usedColors) {
+        if (i >= graph.size())
             return true;
-        }
 
-        Vert vertex = graph.get(index);
+        Vert v = graph.get(i);
         
         Arrays.fill(usedColors, false);
 
-        for (Vert neighbor : vertex.neighbors) {
-            if (neighbor.color > 0) {
+        for (Vert neighbor : v.neighbors)
+            if (neighbor.color > 0)
                 usedColors[neighbor.color] = true;
-            }
-        }
 
         for (int color = 1; color <= k; color++) {
             if (!usedColors[color]) {
-                vertex.color = color;
-                if (colorGraph(graph, index + 1, k, usedColors)) {
+                v.color = color;
+                if (colorGraph(graph, i + 1, k, usedColors))
                     return true;
-                }
             }
         }
 
-        vertex.color = 0;
+        v.color = 0;
         return false;
     }
 
     private static FileData processFile(String filePath) throws FileNotFoundException {
-        FileData fileData = new FileData();
+        FileData data = new FileData();
 
         try (Scanner scanner = new Scanner(new File(filePath))) {
             int n = Integer.parseInt(scanner.nextLine());
-            fileData.k = Integer.parseInt(scanner.nextLine());
-            fileData.graph = new ArrayList<>(n);
+            data.k = Integer.parseInt(scanner.nextLine());
+            data.graph = new ArrayList<>(n);
+
+            for (int i = 0; i < n; i++)
+                data.graph.add(new Vert(i));
 
             for (int i = 0; i < n; i++) {
-                fileData.graph.add(new Vert(i));
-            }
-
-            for (int i = 0; i < n; i++) {
-                Vert v = fileData.graph.get(i);
+                Vert v = data.graph.get(i);
                 String line = scanner.nextLine();
 
                 for (int j = 0; j < line.length(); j++) {
                     if (line.charAt(j) == '1') {
-                        Vert u = fileData.graph.get(j);
+                        Vert u = data.graph.get(j);
                         v.neighbors.add(u);
                         v.degree++;
                     }
                 }
             }
         }
-        return fileData;
+        return data;
     }
 }
